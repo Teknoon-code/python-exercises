6b621a63353b421038d59daa62fe5b6fb16a9c55
Luan Paiva <luanvpaiva2017@gmail.com>
2026-06-24 15:25:01 -0300

    Create 05_letter.py

---
+++ 01-python-basics/05_letter.py
@@ -0,0 +1,11 @@
+#For Luan of the future
+
+print(' :3 ')
+
+print(' Are you having difficulties at the beginning, feeling frustrated')
+print(' Stupid or even incapable of doing ADS')
+print(' But know that your willpower is what keeps you standing')
+print(' Your persistence will take you to unimaginable places')
+print(' To what extent will your creativity and will allow you to go')
+print(' Today, June 17th, a new training cycle begins for Luan.')
+print(' You are amazing <3 ')
